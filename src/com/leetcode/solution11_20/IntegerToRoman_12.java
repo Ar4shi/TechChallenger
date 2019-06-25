d5084c10ea023075e38ee0c6010afbd488b35bfb
Arashi <506428385@qq.com>
2019-06-25 19:41:59 +0800

    1

---
+++ src/com/leetcode/solution11_20/IntegerToRoman_12.java
@@ -0,0 +1,21 @@
+package com.leetcode.solution11_20;
+
+/**
+ * 整数转罗马数字
+ *
+ * @author jianghanh
+ * @date 2019-06-25 10:57
+ */
+public class IntegerToRoman_12 {
+    public String intToRoman(int num) {
+        String result = "";
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] strings = {"M","CM","D","CD","C","XL","L","XL","X","IX","V","IV","I"};
+        while (num!=0){
+            for(int i =0;i<values.length;i++){
+
+            }
+        }
+        return result;
+    }
+}
