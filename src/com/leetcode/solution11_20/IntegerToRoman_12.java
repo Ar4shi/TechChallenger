62476e37f41699c0cb1e1fd4242594223c220b2d
Arashi <506428385@qq.com>
2019-06-26 13:48:47 +0800

    12、13、14

--- src/com/leetcode/solution11_20/IntegerToRoman_12.java
+++ src/com/leetcode/solution11_20/IntegerToRoman_12.java
@@ -8,14 +8,18 @@ package com.leetcode.solution11_20;
  */
 public class IntegerToRoman_12 {
     public String intToRoman(int num) {
-        String result = "";
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] strings = {"M","CM","D","CD","C","XL","L","XL","X","IX","V","IV","I"};
-        while (num!=0){
-            for(int i =0;i<values.length;i++){
-
+        StringBuffer stringBuffer = new StringBuffer();
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}; //枚举出所有可能的不符合一般性规则(小的数字在大的数字的右边)的数值
+        String[] strings = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};//上述数值对应的字符
+        for (int i = 0; i < values.length; i++) {
+            int count = num / values[i];
+            if (count != 0) {
+                for (int j = 0; j < count; j++) {
+                    stringBuffer.append(strings[i]);
+                    num -= values[i];
+                }
             }
         }
-        return result;
+        return stringBuffer.toString();
     }
 }
