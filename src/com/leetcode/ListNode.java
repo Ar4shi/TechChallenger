529b1512df3641ba2a84513933a287d6293cfb0e
Arashi <506428385@qq.com>
2019-06-28 10:15:43 +0800

    链表类优化

--- src/com/leetcode/ListNode.java
+++ src/com/leetcode/ListNode.java
@@ -7,10 +7,34 @@ package com.leetcode;
  * @date 2019-06-27 10:08
  */
 public class ListNode {
-   public int val;
-   public ListNode next;
+    public int val;
+    public ListNode next;
 
-   public ListNode(int x) {
+    public ListNode(int x) {
         val = x;
+    }
+
+    public ListNode(Integer[] nums) {
+        ListNode currNode = this;
+        currNode.val = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currNode.next = new ListNode(nums[i]);
+            currNode = currNode.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        ListNode currNode = this;
+        StringBuilder s = new StringBuilder();
+        while (currNode != null) {
+            s.append(currNode.val);
+            s.append(" -> ");
+            currNode = currNode.next;
+        }
+        // 最后添加一个 NULL 标志表示添加到末尾了
+        s.append("NULL");
+        System.out.println(s.toString());
+        return s.toString();
     }
 }
