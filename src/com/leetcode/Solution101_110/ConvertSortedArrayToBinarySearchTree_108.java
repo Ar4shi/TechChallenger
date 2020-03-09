8dffc806023dfdf9b397b44b4c0140647d8cc7c9
Arashi <506428385@qq.com>
2020-03-09 14:51:27 +0800

    更新题解108

--- src/com/leetcode/Solution101_110/ConvertSortedArrayToBinarySearchTree_108.java
+++ src/com/leetcode/Solution101_110/ConvertSortedArrayToBinarySearchTree_108.java
@@ -9,7 +9,27 @@ import com.leetcode.TreeNode;
  * @date 2020-03-07 16:42
  */
 public class ConvertSortedArrayToBinarySearchTree_108 {
+
+    int[] nums;
+
     public TreeNode sortedArrayToBST(int[] nums) {
-        return null;
+        this.nums = nums;
+        return DFS(0,nums.length-1);
+    }
+
+    /**
+     * 二叉搜索树的中序遍历
+     * 便是升序数组
+     */
+    private TreeNode DFS(int left,int right){
+        TreeNode treeNode = null;
+        if(left>right){
+            return treeNode;
+        }else{
+            treeNode = new TreeNode(nums[(left+right)/2]);
+            treeNode.left = DFS(left,(left+right)/2-1);
+            treeNode.right = DFS((left+right)/2+1,right);
+        }
+        return treeNode;
     }
 }
