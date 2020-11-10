a05dbc2d62c0dc997ce7fd41e364a0541d5b15e7
Arashi <506428385@qq.com>
2020-11-11 01:49:03 +0800

    更新题解

--- src/com/leetcode/Solution01_100/Solution31_40/NextPermutation_31.java
+++ src/com/leetcode/Solution01_100/Solution31_40/NextPermutation_31.java
@@ -8,19 +8,56 @@ package com.leetcode.Solution01_100.Solution31_40;
  */
 public class NextPermutation_31 {
     public void nextPermutation(int[] nums) {
-        if(nums.length>1) {
-            for(int i = 0 ;i<nums.length-2;i++){
-
+        //反向遍历数组遇到的第一个非升序排列的数字下标
+        int firstNumIndex = nums.length - 2;
+        //依然反向遍历数组,遇到的第一个比firstNum大的数字的下标,因为是升序的所以它比nums[firstNumIndex]大一丢丢
+        int secondNumIndex = nums.length - 1;
+        boolean flag = true;
+        for (int i = firstNumIndex; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                firstNumIndex = i;
+                flag = false;
+                break;
             }
         }
+        //正向遍历数组为降序,就是最大的数组,直接倒置
+        if (flag) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        for (int i = secondNumIndex; i > 0; i--) {
+            if (nums[i] > nums[firstNumIndex]) {
+                secondNumIndex = i;
+                break;
+            }
+        }
+        swapNum(nums, firstNumIndex, secondNumIndex);
+        //倒置交换后firstNumIndex所在的数组段,由降序改为升序,此时相比之前增加最少
+        reverse(nums, firstNumIndex + 1, nums.length - 1);
     }
 
     /**
      * 交换数组的两个数字的位置
      */
-    private void swapNum(int[] nums,int i,int j){
+    private void swapNum(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    /**
+     * 倒置数组的某个区间
+     */
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swapNum(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        NextPermutation_31 nextPermutation_31 = new NextPermutation_31();
+        nextPermutation_31.nextPermutation(new int[]{2, 3, 1});
     }
 }
