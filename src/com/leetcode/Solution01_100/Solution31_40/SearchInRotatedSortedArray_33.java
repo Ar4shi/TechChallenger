17edd32e9fc3f0b04c657cae72e125616dc2b2b8
jianghan <jiang.han588@chinaredstar.com>
2020-11-19 19:49:01 +0800

    更新题解

--- src/com/leetcode/Solution01_100/Solution31_40/SearchInRotatedSortedArray_33.java
+++ src/com/leetcode/Solution01_100/Solution31_40/SearchInRotatedSortedArray_33.java
@@ -8,6 +8,38 @@ package com.leetcode.Solution01_100.Solution31_40;
  */
 public class SearchInRotatedSortedArray_33 {
     public int search(int[] nums, int target) {
-        return 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (right - left) >> 1;
+            //翻转点在左区间
+            if (nums[left] > nums[mid]) {
+                //target 在 左区间
+                if (nums[left] >= target && nums[mid] <= target) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {//翻转点在右区间
+                //target 在右区间
+                if (nums[right] >= target && nums[mid] <= target) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray_33 searchInRotatedSortedArray_33 = new SearchInRotatedSortedArray_33();
+        searchInRotatedSortedArray_33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0);
     }
 }
