ce8743fa06dfcec272a7cd835508c680c12b328d
Arashi <506428385@qq.com>
2020-11-20 00:59:32 +0800

    更新题解

--- src/com/leetcode/Solution01_100/Solution31_40/SearchInRotatedSortedArray_33.java
+++ src/com/leetcode/Solution01_100/Solution31_40/SearchInRotatedSortedArray_33.java
@@ -8,38 +8,43 @@ package com.leetcode.Solution01_100.Solution31_40;
  */
 public class SearchInRotatedSortedArray_33 {
     public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
         int left = 0;
         int right = nums.length - 1;
-        while (left < right) {
-            int mid = (right - left) >> 1;
-            //翻转点在左区间
-            if (nums[left] > nums[mid]) {
-                //target 在 左区间
-                if (nums[left] >= target && nums[mid] <= target) {
-                    right = mid;
+        while (left <= right) {
+            int mid = (right + left) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //左半区未反转
+            if (nums[0] <= nums[mid]) {
+                //在左半区
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
                 } else {
-                    left = mid;
+                    //在右半区
+                    left = mid + 1;
                 }
-            } else {//翻转点在右区间
-                //target 在右区间
-                if (nums[right] >= target && nums[mid] <= target) {
-                    left = mid;
+            } else {//左半区有反转
+                //在右半区
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    left = mid + 1;
                 } else {
-                    right = mid;
+                    //在左半区
+                    right = mid - 1;
                 }
             }
-        }
-        if (nums[left] == target) {
-            return left;
-        }
-        if (nums[right] == target) {
-            return right;
         }
         return -1;
     }
 
     public static void main(String[] args) {
         SearchInRotatedSortedArray_33 searchInRotatedSortedArray_33 = new SearchInRotatedSortedArray_33();
-        searchInRotatedSortedArray_33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0);
+        searchInRotatedSortedArray_33.search(new int[]{1, 3, 5}, 1);
     }
 }
