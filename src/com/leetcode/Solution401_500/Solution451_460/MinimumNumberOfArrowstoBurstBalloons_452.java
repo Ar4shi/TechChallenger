07bebaec956058afaf8fec6d2ab012f44ae588cc
Arashi <506428385@qq.com>
2020-11-25 00:23:31 +0800

    更新题解

--- src/com/leetcode/Solution401_500/Solution451_460/MinimumNumberOfArrowstoBurstBalloons_452.java
+++ src/com/leetcode/Solution401_500/Solution451_460/MinimumNumberOfArrowstoBurstBalloons_452.java
@@ -1,5 +1,7 @@
 package com.leetcode.Solution401_500.Solution451_460;
 
+import java.util.Arrays;
+
 /**
  * 用最少数量的箭引爆气球
  *
@@ -8,17 +10,20 @@ package com.leetcode.Solution401_500.Solution451_460;
  */
 public class MinimumNumberOfArrowstoBurstBalloons_452 {
     public int findMinArrowShots(int[][] points) {
-        return 0;
-    }
-
-    private int[] getOverlapArray(int[] a, int[] b) {
-        int aLeft = a[0];
-        int aRight = a[1];
-        int bLeft = b[0];
-        int bRight = b[1];
-        if(bRight<aLeft){
-
+        if (points.length == 0) return 0;
+        Arrays.sort(points, (p1, p2) -> p1[0] < p2[0] ? -1 : 1);
+        int start = points[0][0], end = points[0][1];
+        int count = 1;
+        for (int i = 1; i < points.length; ++i) {
+            if (points[i][0] <= end) {
+                start = Math.max(points[i][0], start);
+                end = Math.min(points[i][1], end);
+            } else {
+                count++;
+                start = points[i][0];
+                end = points[i][1];
+            }
         }
-        return null;
+        return count;
     }
 }
