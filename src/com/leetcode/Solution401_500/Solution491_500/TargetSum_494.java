9075c84f8691a5146dd9d2355187d8bcd9041475
Arashi <506428385@qq.com>
2021-01-19 02:36:55 +0800

    更新题解

--- src/com/leetcode/Solution401_500/Solution491_500/TargetSum_494.java
+++ src/com/leetcode/Solution401_500/Solution491_500/TargetSum_494.java
@@ -8,23 +8,22 @@ package com.leetcode.Solution401_500.Solution491_500;
  */
 public class TargetSum_494 {
     public int findTargetSumWays(int[] nums, int S) {
-        //dp[i][j] 表示取nums的前i位,能组合成数字j的方案数
-        int[][] dp = new int[nums.length + 1][2001];
-        dp[1][nums[0] + 1000] = 1;
-        for (int i = 1; i < nums.length + 1; i++) {
-            for (int j = -1000; j <= 1000; j++) {
-                //和走楼梯那个逻辑差不多
-                //dp[i][j + 1000] = dp[i - 1][j - nums[i] + 1000] + dp[i - 1][j + nums[i] + 1000];
-                if (j - nums[i] > 1000) {
-                    dp[i][j + 1000] = dp[i - 1][j - nums[i] + 1000];
+        int[][] dp = new int[nums.length][2001];
+        //dp[i][j] 表示 取出nums[i]之前包括nums[i]个数,其和为j-1000的解法的个数
+        //由于java中数组下标不能为负数,所以统一加上1000(题目给出的最大和)
+        dp[0][nums[0] + 1000] = 1;
+        dp[0][-nums[0] + 1000] += 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int sum = -1000; sum <= 1000; sum++) {
+                if (dp[i - 1][sum + 1000] > 0) {
+                    //dp[ i ][ j ] = dp[ i - 1 ][ j - nums[ i ] ] + dp[ i - 1 ][ j + nums[ i ] ]
+                    //令J = sum + nums[i] + 1000 ,带入上式,这一步单纯的是代数计算,如果不好理解可以把上式拆成2个等式分别做if处理
+                    dp[i][sum + nums[i] + 1000] += dp[i - 1][sum + 1000];
+                    dp[i][sum - nums[i] + 1000] += dp[i - 1][sum + 1000];
                 }
-                if (j + nums[i] < 2000) {
-                    dp[i][j + 1000] += dp[i - 1][j + nums[i] + 1000];
-                }
-
             }
         }
-        return dp[nums.length][S + 1000];
+        return S > 1000 ? 0 : dp[nums.length - 1][S + 1000];
     }
 
     public static void main(String[] args) {
