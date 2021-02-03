b032372545147e290972d7557108a6543a081a35
Arashi <506428385@qq.com>
2021-02-04 02:35:22 +0800

    更新题解

---
+++ src/com/leetcode/Solution01_100/Solution41_50/Pow_50.java
@@ -0,0 +1,13 @@
+package com.leetcode.Solution01_100.Solution41_50;
+
+/**
+ * Pow(x, n)
+ *
+ * @author jianghan
+ * @date 2021-02-02 01:49
+ */
+public class Pow_50 {
+    public double myPow(double x, int n) {
+        return 0;
+    }
+}
