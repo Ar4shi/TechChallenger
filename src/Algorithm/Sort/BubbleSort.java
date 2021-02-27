2048fb3c68f0ef4dbd048e3f28b58a856290f985
Arashi <506428385@qq.com>
2021-02-28 00:04:06 +0800

    更新冒泡排序注释

--- src/Algorithm/Sort/BubbleSort.java
+++ src/Algorithm/Sort/BubbleSort.java
@@ -10,8 +10,17 @@ package Algorithm.Sort;
  */
 public class BubbleSort {
 
+    /**
+     * 冒泡排序是【原地】地进行排序
+     * 主要思路是通过不停地比较相邻的两个数的大小,使得较大的那个数往数组的末端移动
+     * 这个过程就好像泡泡上浮由于水压变低越来越大一样,所以称为冒泡排序
+     * 通过不停的循环冒泡每次都能使大数上浮直到扫描到唯一的元素不需要上浮排序也就完成了
+     */
     public void bubble_sort(int[] array) {
+        //优化点1 N个数循环N-1次就够了
         for (int i = 0; i < array.length - 1; i++) {
+            //优化点2 由于每一次排序后,最大的数都会上浮到对应的位置
+            //所以每次循环都可以节约相应的比对次数,比对过程会越来越少
             for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
