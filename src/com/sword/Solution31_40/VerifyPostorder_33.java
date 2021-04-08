05ae4be40ef5e7a162abf4f589458b8839e4ca87
Arashi <506428385@qq.com>
2021-04-09 03:05:10 +0800

    剑指Offer

--- src/com/sword/Solution31_40/VerifyPostorder_33.java
+++ src/com/sword/Solution31_40/VerifyPostorder_33.java
@@ -17,13 +17,18 @@ public class VerifyPostorder_33 {
     }
 
     private void recursion(int left, int right) {
-        int maxIndex = findMaxIndex(left, right);
-        if (maxIndex == -1 || !result) {
+        int rootIndex = findRootIndex(left, right);
+        if (rootIndex == -1 || !result) {
             return;
         }
-        this.result = judge(left, right, maxIndex);
-        recursion(left, maxIndex);
-        recursion(maxIndex, right);
+        this.result = judge(left, right, rootIndex);
+        if (rootIndex == -2) {
+            //没有比末尾大的树 即都是他的左子树
+            recursion(left, right - 1);
+        }else {
+            recursion(left, rootIndex - 1);
+            recursion(rootIndex, right - 1);
+        }
     }
 
 
@@ -31,15 +36,26 @@ public class VerifyPostorder_33 {
      * 判断左子树的值是否小于根
      * 右子树的值是否大于根
      */
-    private boolean judge(int left, int right, int maxIndex) {
-        for (int i = left; i <= maxIndex; i++) {
-            if (postorder[i] > postorder[maxIndex]) {
-                return false;
+    private boolean judge(int left, int right, int rootIndex) {
+        //末尾数 即子树的根节点
+        int last = this.postorder[right];
+        //没有比末尾大的树 即都是他的左子树
+        if (rootIndex == -2) {
+            for (int i = left; i < right; i++) {
+                if (postorder[i] > last) {
+                    return false;
+                }
             }
-        }
-        for (int i = maxIndex; i <= right; i++) {
-            if (postorder[i] < postorder[maxIndex]) {
-                return false;
+        } else {
+            for (int i = left; i < rootIndex; i++) {
+                if (postorder[i] > last) {
+                    return false;
+                }
+            }
+            for (int i = rootIndex; i < right; i++) {
+                if (postorder[i] < last) {
+                    return false;
+                }
             }
         }
         return true;
@@ -49,23 +65,24 @@ public class VerifyPostorder_33 {
      * 找出数组中第一个比末尾大的数的索引
      * 该索引划分了左右子树
      */
-    private int findMaxIndex(int left, int right) {
-        //末尾数 即子树的根节点
+    private int findRootIndex(int left, int right) {
         int res = -1;
-        int last = this.postorder[right];
-        if (left >= right) {
+        if (left >= right || left < 0) {
             return res;
         }
+        //末尾数 即子树的根节点
+        int last = this.postorder[right];
         for (int i = left; i < right; i++) {
             if (this.postorder[i] > last) {
-
+                return i;
             }
         }
-        return res;
+        //没有比末尾大的树 即都是他的左子树
+        return -2;
     }
 
     public static void main(String[] args) {
         VerifyPostorder_33 verifyPostorder_33 = new VerifyPostorder_33();
-        verifyPostorder_33.verifyPostorder(new int[]{4, 8, 6, 12, 16, 14, 10});
+        System.out.println(verifyPostorder_33.verifyPostorder(new int[]{5, 2, -17, -11, 25, 76, 62, 98, 92, 61}));
     }
 }
