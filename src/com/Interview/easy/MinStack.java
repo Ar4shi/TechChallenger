db65c5f794269b11741501f6693edeb47d1ff17a
Ar4shi <506428385@qq.com>
2024-01-08 02:37:57 +0800

    update

---
+++ src/com/Interview/easy/MinStack.java
@@ -0,0 +1,38 @@
+package com.Interview.easy;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 请设计一个栈，除了常规栈支持的pop与push函数以外，还支持min函数，该函数返回栈元素中的最小值。执行push、pop和min操作的时间复杂度必须为O(1)。
+ */
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
