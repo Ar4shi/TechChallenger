108dccd14f8f351ee935466da8d37aecb89307c4
Ar4shi <506428385@qq.com>
2024-01-07 21:20:04 +0800

    update

---
+++ src/com/Interview/medium/LinkedListCycle.java
@@ -0,0 +1,47 @@
+package com.Interview.medium;
+
+import com.leetcode.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。若环不存在，请返回 null。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ */
+public class LinkedListCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        Set<ListNode> nodeSet = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (nodeSet.contains(node.next)) {
+                return node.next;
+            }
+            nodeSet.add(node);
+            node = node.next;
+        }
+        return null;
+    }
+}
