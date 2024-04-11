0f8c8183213efa32c3d1988f997871c0870123c1
han.jiang <han.jiang@trip.com>
2024-04-11 14:34:04 +0800

    Update Daily problem solutions

---
+++ src/main/java/com/interview/medium/GroupAnagrams.java
@@ -0,0 +1,37 @@
+package com.interview.medium;
+
+import java.util.*;
+
+/**
+ * 编写一种方法，对字符串数组进行排序，将所有变位词组合在一起。变位词是指字母相同，但排列不同的字符串。
+ * 注意：本题相对原题稍作修改
+ * <p>
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * <p>
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            // 对字符数组进行排序
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
