87f3d3f4a47c7cfcda82ece0484d51ed0b9a5d7c
han.jiang <han.jiang@trip.com>
2024-05-13 19:14:57 +0800

    Daily Commit For Studying

--- src/main/java/com/leetcode/Solution01_100/solution01_10/MedianValue_04.java
+++ src/main/java/com/leetcode/Solution01_100/solution01_10/MedianValue_04.java
@@ -1,10 +1,26 @@
 package com.leetcode.Solution01_100.solution01_10;
 
+
 /**
- * 寻找两个有序数组的中位数
- *
- * @author jianghan
- * @date 2019-06-16 22:32
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。
+ * <p>
+ * 算法的时间复杂度应该为 O(log (m+n)) 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * <p>
+ * 输出：2.00000
+ * <p>
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * <p>
+ * 输出：2.50000
+ * <p>
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
  */
 public class MedianValue_04 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
